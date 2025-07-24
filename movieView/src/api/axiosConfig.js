284a75d88bdc5620ff81fee25340a8eb01cc920a
Mark Chen <201905190410@smail.xtu.edu.cn>
2025-07-24 11:22:54 +0800

    Update axiosConfig.js

--- movieView/src/api/axiosConfig.js
+++ movieView/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL:'http://localhost:8080',
+    baseURL:'http://20070809.xyz:8080',
     headers: {"ngrok-skip-browser-warning": "true"}
 })
